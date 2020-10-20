b8d1607c29b7f3cac824f1ce6bbbd253bbcf3fe7
Nikita Borovikov <borumchik@gmail.com>
2020-10-20 11:30:32 +0200

    Updates

---
+++ precache-manifest.c9aa99281fafeb8638986866c5967d33.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e01fee6d457b790cd9eb2d0876902d90",
+    "url": "/kode-task-2/index.html"
+  },
+  {
+    "revision": "336844cd867c6d88f501",
+    "url": "/kode-task-2/static/css/main.afc59e71.chunk.css"
+  },
+  {
+    "revision": "447f0e6ab4bfcdc562ff",
+    "url": "/kode-task-2/static/js/2.106f5b7d.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/kode-task-2/static/js/2.106f5b7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "336844cd867c6d88f501",
+    "url": "/kode-task-2/static/js/main.d35122be.chunk.js"
+  },
+  {
+    "revision": "435f5d9b7a66eec6c27a",
+    "url": "/kode-task-2/static/js/runtime-main.d919417b.js"
+  },
+  {
+    "revision": "2090aeccce713476ee4fde0f300c1951",
+    "url": "/kode-task-2/static/media/arrow.2090aecc.svg"
+  },
+  {
+    "revision": "8bc686b178e994a456f61cb8ed7f3d42",
+    "url": "/kode-task-2/static/media/pikachu.8bc686b1.gif"
+  },
+  {
+    "revision": "d51f9f9be24c18d0f4ec704c3a8f8458",
+    "url": "/kode-task-2/static/media/pokemon-not-found.d51f9f9b.jpg"
+  }
+]);
